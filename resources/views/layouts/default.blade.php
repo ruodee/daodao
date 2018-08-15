17b7033bb837f00461066f7f7e889f4adad4531d
vagrant <vagrant@homestead>
2018-08-15 06:38:06 +0000

    Finish static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>@yield('title','论坛') - daodaoBBS</title>
+    <link rel="stylesheet" href="">
+</head>
+<body>
+        @yield('content')
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,6 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','StaticPagesController@home')->name('home');
+Route::get('/help','StaticPagesController@help');
+Route::get('/about','StaticPagesController@about');
