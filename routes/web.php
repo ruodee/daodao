a9074d5c3d645e403242cadb71a6dc27cd8672cd
zhangbin <ruodee@126.com>
2018-08-19 02:04:31 +0000

    Move user model to models folder

--- routes/web.php
+++ routes/web.php
@@ -10,15 +10,12 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-<<<<<<< HEAD
 
 Route::get('/', function () {
     return view('welcome');
 });
-=======
 Route::get('/','StaticPagesController@home')->name('home');
 Route::get('/help','StaticPagesController@help')->name('help');
 Route::get('/about','StaticPagesController@about')->name('about');
 
 Route::get('sigup','UsersController@create')->name('signup');
->>>>>>> ba339aa8de7379921aeaa2520a21f98f6a9778b2
