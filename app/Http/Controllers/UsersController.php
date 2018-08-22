be5ec0dfbc3939f6559c15df5bf28b678752be0e
zhangbin <ruodee@126.com>
2018-08-22 22:54:37 +0000

    user edit

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -41,4 +41,26 @@ class UsersController extends Controller
         session()->flash('success','欢迎，注册成功了，您将在这里扬帆起航！');
         return redirect()->route('users.show',[$user]);
     }
+
+    public function edit(User $user)
+    {
+        return view('users.edit',compact('user'));
+    }
+
+    public function update(User $user,Request $request)
+    {
+        $this->validate($request,[
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirm|min:6'
+        ]);
+        $data = [];
+        $data['name'] = $request->name;
+        if($request->password)
+            $data['password'] = bcrypt($request->password);
+        $user->update($data);
+
+        session()->flash('sucess','个人资料更新成功！');
+        return redirect()->route('users.show',$user->id);
+
+    }
 }
